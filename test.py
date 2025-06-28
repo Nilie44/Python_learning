22cbc24e44ff26f5645f39d87e75169d76fceb28
NILIE <nilienganka@gmail.com>
2025-06-28 14:01:09 +0200

    test de la fonction print et l'utilisation des variable

---
+++ test.py
@@ -0,0 +1,28 @@
+print("hello, world!")
+
+A= (100 + 81) * 11
+print(A)
+livre = "Gatsby le Magnifique"
+print(livre)
+
+nom = "Nilie"
+age = 28
+taille = 1.64
+Est_etudiante = True
+
+
+print(f"je m'appelle {nom},je compte avoir ma première fille a {age}")
+
+age = age + 10
+print(f"je m'appelle {nom},je compte avoir ma première fille a {age}")
+
+#affichage des variables
+print(f"Nom: {nom}")
+print(f"Taille: {taille}")
+print(f"Est_etudiante: {Est_etudiante}")
+
+#affichage des types des variables
+
+print(f"Type nom: {type(nom)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est etudiante: {type(Est_etudiante)}")
